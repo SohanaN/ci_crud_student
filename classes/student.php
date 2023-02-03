9cb980486adbb1f3d4fe4613ee0015a167283d67
SohanaN <sohananaz01@gmail.com>
2023-02-04 05:04:01 +0600

    first commit

---
+++ classes/student.php
@@ -0,0 +1,41 @@
+<?php
+
+class Student {
+
+    //put your code here
+
+    public function __construct() {
+        $hostname = 'localhost';
+        $username = 'root';
+        $password = '';
+        $db_name = 'db_ci_oop_crud_tt';
+        $this->conn = mysqli_connect($hostname, $username, $password, $db_name);
+        if (!$this->conn) {
+            die("sever not connected");
+        } else {
+//            echo "Database Sever connected";
+        }
+    }
+
+//    for add page
+    public function save_student($data) {
+//        echo '<pre>';
+//        print_r($data);
+        $sql = "INSERT INTO tbl_student (student_name,email_address,mobile_number)VALUES('$data[student_name]','$data[email_address]','$data[mobile_number]')";
+        mysqli_query($this->conn, $sql);
+    }
+
+//    for view page
+    public function select_all_student() {
+        $sql = "SELECT * FROM tbl_student"; // * all data
+//        $sql = "SELECT student_name,mobile_number FROM tbl_student"; // jei jei data lagbe seta
+        $result = mysqli_query($this->conn, $sql);
+        /*
+          echo '<pre>';
+          print_r($result); // array ke echo kora jay na print kore dekhte hoy
+          exit();
+         * */
+        return $result;
+    }
+
+}
---
+++ view_student.php
@@ -0,0 +1,60 @@
+
+<?php
+require './classes/student.php';
+$obj = new Student();
+
+$result = $obj->select_all_student();
+?>
+<html>
+    <head>
+        <title>View Student</title>
+    </head>
+    <body>
+        <table border="1" align="center" width="980px" height="1100px">
+            <tr>
+                <td width="150px" height="150px">Logo</td>
+                <td colspan="2">banner</td>
+            </tr>
+            <tr>
+                <td colspan="3" height="50px">
+                    <a href="add_student.php">Add Student</a> |
+                    <a href="view_student.php">View Student</a>
+                </td>
+            </tr>
+            <tr>
+                <td>left</td>
+                <td valign="top">
+                    <h3 align="center">All Student's</h3>
+                    <hr/>
+                    <table border="1" align="center" width="90%">
+                        <tr>
+                            <th>Id</th>
+                            <th>Student Name</th>
+                            <th>Email</th>
+                            <th>Mobile</th>
+                            <th>Action</th>
+                        </tr>
+                        <?php
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            ?>
+                            <tr>
+                                <td><?php echo $row['student_id'] ?></td>
+                                <td><?php echo $row['student_name'] ?></td>
+                                <td><?php echo $row['email_address'] ?></td>
+                                <td><?php echo $row['mobile_number'] ?></td>
+                                <td>
+                                    <a href="">Edit</a> |
+                                    <a href="">Delete</a>
+                                </td>
+                            </tr>
+                        <?php } ?>
+                    </table>
+                </td>
+                <td width="150px">right</td>
+            </tr>
+            <tr>
+                <td colspan="3" height="80px">footer</td>
+            </tr>
+        </table>
+    </body>
+</html>
